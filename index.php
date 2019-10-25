736ddb83e14b494530d8b6d52deab781a8ade7b8
IsabelAguera <isabel.aguera.c@gmail.com>
2019-10-25 09:50:00 +0200

    index

--- index.php
+++ index.php
@@ -36,26 +36,16 @@ session_start();
                     // content.innerHTML="<img src=http://image.tmdb.org/t/p/w185//".concat(test,"></img>");
                     // console.log(test);
                     })
-
                     }
             movie();
             }
-
 </script>
 <?php include('NavBar.php'); ?>
 <!-- Vidéo Principale -->
         <section class='corps'>
-<script>
-
-function movie(){
-    fetch('https://https://api.themoviedb.org/3/discover/movie?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US')
-    .then(reponse =>reponse.json())
-    .then (data => {
-    console.log(data.results['0'])
-})
-
-}
-</script>
+        <iframe src="https://www.youtube.com/embed/RumjcP_9vuo" frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen class="enter">
+        </iframe>
         </section>
 <!-- Fin Vidéo Principale -->
 <!-- Top Film -->
@@ -113,7 +103,8 @@ function movie(){
             </script>
         </section>
         <?php include('footer.php'); ?>
-<script type="type/javascript" src="js.js"></script>
+
+    
 <script src="https://kit.fontawesome.com/75bed6266a.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
