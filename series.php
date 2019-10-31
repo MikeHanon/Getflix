3601ff5d780cd364f65c51f48e6d75eecae06446
pawel <przew.p@gmail.com>
2019-10-31 16:54:49 +0100

    t

--- series.php
+++ series.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 ?>
@@ -15,10 +15,108 @@ session_start();
     <link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
     <title>SeriesAddict</title>
 </head>
-          
+
 <body class='genre'>
-<!--On inclut la NavBar-->
+
+<script>
+            function afficherFilm(nombre,url,id,page){
+                var content=document.getElementById(id);
+                page=1;
+                id="";
+                nombre=6;
+                genre="";
+                url1=url+"&page="+page;
+                function movie(page){
+                    fetch(url)
+                    .then(reponse =>reponse.json())
+                    .then (data => {
+                    for(var i=0;i<nombre;i++){
+                    var test  =data.results[i].poster_path;
+                    var idFilm = data.results[i].id;
+                    content.innerHTML+="<div class='col-md-2' id='movie'><a alt ='"+data.results[i].name+"' class='pochette' href='pageVideo.php?id='><img alt='TV poster' src=http://image.tmdb.org/t/p/w185//".concat(test,"></img> </a><p id='title'>"+data.results[i].name+"</p></div>" );
+                    }
+
+                    })
+                    }
+            movie();
+            }
+
+</script>
 <?php include('NavBar.php'); ?>
+<h2 class="titre">Animation</h2>
+<p class="genreDesc">Anim</p>
+<h4 id="genreTitle">Check our Animation catalogue</h4>
+<!--Carousel Wrapper-->
+<div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+  <!--Controls-->
+  <div class="controls-top">
+    <a id='btnLeft' class="btn-floating" href="#multi-item-example" data-slide="prev"><i class="fas fa-chevron-left"></i></a>
+    <a id ='btnRight' class="btn-floating" href="#multi-item-example" data-slide="next"><i
+        class="fas fa-chevron-right"></i></a>
+  </div>
+  <!--/.Controls-->
+
+  <!--Indicators-->
+  <ol class="carousel-indicators">
+    <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
+    <li data-target="#multi-item-example" data-slide-to="1"></li>
+    <li data-target="#multi-item-example" data-slide-to="2"></li>
+    <li data-target="#multi-item-example" data-slide-to="3"></li>
+    <li data-target="#multi-item-example" data-slide-to="4"></li>
+  </ol>
+  <!--/.Indicators-->
+
+  <!--Slides-->
+  <div class="carousel-inner" role="listbox">
+
+    <!--First slide-->
+    <div class="carousel-item active">
+    <div class="row" id="firstRow">
+        <script> afficherFilm(6," https://api.themoviedb.org/3/discover/tv?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&sort_by=popularity.desc&page=1&include_null_first_air_dates=false",'firstRow'); </script>
+
+    </div>
+    </div>
+    <!--/.First slide-->
+
+    <!--Second slide-->
+    <div class="carousel-item">
+    <div class="row" id="secondRow">
+        <script> afficherFilm(6," https://api.themoviedb.org/3/discover/tv?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&sort_by=popularity.desc&page=2&include_null_first_air_dates=false",'secondRow'); </script>
+
+    </div>
+    </div>
+
+    <!--/.Second slide-->
+
+    <!--Third slide-->
+    <div class="carousel-item">
+    <div class="row" id="thirdRow">
+        <script> afficherFilm(6," https://api.themoviedb.org/3/discover/tv?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&sort_by=popularity.desc&page=3&include_null_first_air_dates=false",'thirdRow'); </script>
+
+    </div>
+    </div>
+
+    <div class="carousel-item">
+    <div class="row" id="fourRow">
+        <script> afficherFilm(6," https://api.themoviedb.org/3/discover/tv?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&sort_by=popularity.desc&page=4&include_null_first_air_dates=false",'fourRow'); </script>
+
+    </div>
+    </div>
+
+    <div class="carousel-item">
+    <div class="row" id="fiveRow">
+        <script> afficherFilm(6," https://api.themoviedb.org/3/discover/tv?api_key=b53ba6ff46235039543d199b7fdebd90&language=en-US&sort_by=popularity.desc&page=5&include_null_first_air_dates=false",'fiveRow'); </script>
+
+    </div>
+    </div>
+
+  </div>
+  <!--/.Slides-->
+
+</div>
+<!--/.Carousel Wrapper-->
+
 
 
 <!--On inclut le footer-->
