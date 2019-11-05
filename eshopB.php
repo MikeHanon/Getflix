ff6c65614e81b2c1aa00fe567d10e14b41cc9195
NicolasTassin <nicolastassin598@gmail.com>
2019-11-05 14:37:33 +0100

    quantité réglable dans commande

--- eshopB.php
+++ eshopB.php
@@ -12,25 +12,38 @@ $result=$statement->fetchAll()
 
 ?>
 <?php include('NavBar.php'); ?>
+<h1 class="m-0 px-5 pt-3">Eshop</h1>
 
+<form action="" class="d-flex flex-column p-5">
 <table>
   <tr>
-    <th>id film</th>
-    <th>quantité</th>
-    <th>prix</th>
+    <th>ID Movie</th>
+    <th>Movie Name</th>
+    <th>Quantity</th>
+    <th>Total Price</th>
   </tr>
 <?php
 foreach($result as $row){?>
   
-  <tr><td><?=$row['id_vid']?></td> 
-  <td><?=$row['qty']?></td> 
-  <td><?=$row['prix'];?></td>;
+  <tr><td><?=$row['id_vid']?></td>
+  <td>Movie name</td>
+  <td><select name="Qty" id="">
+        <option value="1"<?=($row['qty'] === '1' ? 'selected' : '');?>>1</option>
+        <option value="2"<?=($row['qty'] === '2' ? 'selected' : '');?>>2</option>
+        <option value="3"<?=($row['qty'] === '3' ? 'selected' : '');?>>3</option>
+        <option value="4"<?=($row['qty'] === '4' ? 'selected' : '');?>>4</option>
+        <option value="5"<?=($row['qty'] === '5' ? 'selected' : '');?>>5</option>
+        <option value="6"<?=($row['qty'] === '6' ? 'selected' : '');?>>6</option>
+        <option value="7"<?=($row['qty'] === '7' ? 'selected' : '');?>>7</option>
+        <option value="8"<?=($row['qty'] === '8' ? 'selected' : '');?>>8</option>
+        <option value="9"<?=($row['qty'] === '9' ? 'selected' : '');?>>9</option>
+        <option value="10"<?=($row['qty'] === '10' ? 'selected' : '');?>>10</option>
+      </select></td>
+  <td><?=$row['prix'];?></td>
 <?php } ?>
-?>
-</table>
-  <h1 class="m-0 px-5 pt-3">Eshop</h1>
 
-  <form action="" class="d-flex flex-column p-5">
+</table>
+  
     <label for="">Nom</label>
     <input type="text" class="w-50">
     <label for="">Prenom</label>
