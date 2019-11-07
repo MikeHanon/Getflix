50a84c9900dc604267b3b6ffc4e9c8d57bdbfea5
Serbest <bunyamin.srbst@gmail.com>
2019-11-07 13:53:12 +0100

    reductions

--- eshopB2.php
+++ eshopB2.php
@@ -15,14 +15,14 @@ $result=$statement->fetchAll();
 
 ?>
 
-<h2 class="px-5">Commande Validé</h2>
 
-<h3 class="px-5">Vos données :</h3>
+
+<h3 class="px-5">Your personal information :</h3>
 
 
 <?php
 
- echo "<section class='px-5'>" . "<b> Commmande au nom de : </b> " . $_POST['nom'] . " " . $_POST['prenom'] . "<br>" . "<b> Adresse de livraison: </b> " . $_POST['adresse'] . "</section>" . "</br>";
+ echo "<section class='px-5'>" . "<b> Order in the name of : </b> " . $_POST['nom'] . " " . $_POST['prenom'] . "<br>" . "<b> Delivery address: </b> " . $_POST['adresse'] . "</section>" . "</br>";
  
  if ($_POST['codePromo']=="MikeEstTropCool" ){
   echo "<span class='px-5'> <b> REDUCTION 10% ACTIVÉ </b> </span>";
@@ -31,17 +31,21 @@ $result=$statement->fetchAll();
 } 
 ?>
  
-<h3 class="px-5">Récapitulatif de votre commande</h3>
+<h3 class="px-5">Overview of your order</h3>
 
 <table>
 <tr>
 <th class="pl-5">Id Film</th>
-<th>Quantité</th>
-<th>Prix</th>
+<th>Quantity</th>
+<th>Price</th>
 </tr>
 
 <?php
+$price =0;
+$qty =0;
 foreach($result as $row){
+$qty += intval(($row['qty']));
+$price += intval(($row['prix']));
 ?>
 
 <tr>
@@ -53,9 +57,45 @@ foreach($result as $row){
 
 </tr>
 </table>
+
+<?php if ($qty>=5 && $_POST['codePromo']=="MikeEstTropCool"){
+  echo "<p class='px-5'> 10% discount for your promoCode </p>". "<br>";
+  echo "<p class='px-5'> 5% discount for 5 or more than 5 dvd or blue-ray bought. </p>". "<br>";
+  echo "<p class='px-5'> Total price of your order : " . ($price-($price/100*15)) . "€ </p>";
+}elseif($qty>=5){
+  echo "<p class='px-5'> 5% discount for 5 or more than 5 dvd or blue-ray bought. </p>". "<br>";
+  echo "<p class='px-5'> Total price of your order : " . ($price-($price/100*5)) . "€ </p>";
+}elseif($_POST['codePromo']){
+  echo "<p class='px-5'> 10% discount for your promoCode </p>". "<br>";
+  echo "<p class='px-5'> Total price of your order : " . ($price-($price/100*10)) . "€ </p>";
+}else{
+  echo "<p class='px-5'> Total price of your order : " .$price."€ </p>";
+}
+?>
 <div class="pl-5 py-3">
-<a href="eshopC.php"><button >Confirmer commande</button></a>
+
+<a href="eshopC.php"><button >Confirm your order</button></a>
 </div>
+
+
+
+
+
 
 <?php include('footer.php'); ?>
 
+
+
+<!--
+<?php
+$price =0;
+foreach($result as $row){
+  $price += intval(($row['prix']));
+  
+ 
+}
+echo $price;
+
+?>
+
+-->
